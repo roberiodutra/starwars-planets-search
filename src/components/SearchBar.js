38b2565a542c2a0cb3a1147bf6faaac4f55cea31
Robério Almeida <dev.roberio@gmail.com>
2022-04-17 21:01:45 -0300

    feat: cria component SearchBar
    component para fazer as buscas, nele serão inseridos os filtros solicitados pelo cliente e aplicados diretamente na tabela

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <section>
+      <h2>Planet Search</h2>
+    </section>
+  );
+}
+
+export default SearchBar;
--- src/components/Table.js
+++ src/components/Table.js
@@ -35,24 +35,23 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {data && data
-          .map((planet) => (
-            <tr key={ planet.name }>
-              <td>{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.population}</td>
-              <td>{planet.films}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-              <td>{planet.url}</td>
-            </tr>
-          ))}
+        {data && data.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
