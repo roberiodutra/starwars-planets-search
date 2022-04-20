7b53f4170e9ea5f708cd319348ce70b9e60fe8b1
Robério Almeida <dev.roberio@gmail.com>
2022-04-20 01:08:05 -0300

    feat: criação de super button
    button para limpar todos os filtros e resetar as colunas, finalizando requisito 6

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from '../contexts/PlanetsContext';
 import FilterName from '../filters/FilterName';
 import FilterNumber from '../filters/FilterNumber';
 import AppliedFilters from './AppliedFilters';
 
 function SearchBar() {
+  const { setFilter, setFilteredColumns } = useContext(PlanetsContext);
+
+  function clearFilters() {
+    setFilter({
+      filterByName: { name: '' },
+      filterByNumericValues: [],
+    });
+    setFilteredColumns([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+  }
+
   return (
     <section>
       <FilterName />
       <FilterNumber />
       <AppliedFilters />
+      <button
+        type="button"
+        data-testid="button-remove-filters"
+        onClick={ clearFilters }
+      >
+        Remove Filters
+      </button>
     </section>
   );
 }
