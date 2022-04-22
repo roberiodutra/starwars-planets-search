8d0e9f7881144561604be2baa4e9078c0eb3e2c1
Robério Almeida <dev.roberio@gmail.com>
2022-04-21 22:50:09 -0300

    feat: adiciona component Header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Provider from './contexts/Provider';
 import Table from './components/Table';
 import SearchBar from './components/SearchBar';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <Provider>
+      <Header />
       <SearchBar />
       <Table />
     </Provider>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Star Wars - Planet Search</h1>
+    </header>
+  );
+}
+
+export default Header;
